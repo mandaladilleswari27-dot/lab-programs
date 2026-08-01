84afdbc433efd80bd108256379a0c6d3901b1b78
mandaladilleswari27-dot <mandaladilleswari27@gmail.com>
2026-08-01 22:42:26 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,10 @@
+public class Sum{
+void display(){
+    int a=10,b=20;
+    int c=a+b;
+    System.out.println("result:" + c);}
+    public static void main(String[] args){
+        Sum t=new Sum();
+        t.display();
+    }
+}

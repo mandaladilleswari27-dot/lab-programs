f5b22b0fbcdd1bf0e9143babfcd1b84674a3870d
mandaladilleswari27-dot <mandaladilleswari27@gmail.com>
2026-08-01 22:51:57 +0530

    Add files via upload

---
+++ Singleinheritance.java
@@ -0,0 +1,20 @@
+class Addition {
+    int x=10;
+    int y=20;
+    void add() {
+    int sum=x+y;
+    System.out.println("Sum =" + sum);
+    }
+}
+class Result extends Addition{
+    void display() {
+        add();
+    }
+
+} 
+public class Singleinher {
+public static void main(String[] args) {
+    Result r= new Result();
+    r.display();
+}
+}

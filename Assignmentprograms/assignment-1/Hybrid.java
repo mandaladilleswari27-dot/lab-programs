fa37cae29e5baf27702ce6c0487f7bf0b99d11ef
mandaladilleswari27-dot <mandaladilleswari27@gmail.com>
2026-08-14 08:21:48 +0530

    Update Hybrid.java

--- Assignmentprograms/assignment-1/Hybrid.java
+++ Assignmentprograms/assignment-1/Hybrid.java
@@ -11,15 +11,15 @@ class p1{
 		 System.out.println("see :"+super.x+ " "+this.y);
 	 }
  }
-// class p3 extends p2{
-//	 int z=12;
-//	 void demo() {
-//		 System.out.println("see :"+super.x+ " "+super.y+ " "+this.z);
-//	 }
-// }
- class p3 extends p1{
+class p3 extends p2{
 	 int z=12;
 	 void demo() {
+		 System.out.println("see :"+super.x+ " "+super.y+ " "+this.z);
+	 }
+}
+ class p4 extends p1{
+	 int z=12;
+	 void demos() {
 		 System.out.println("see :"+super.x+ " "+this.z);
 	 }
  }
@@ -27,5 +27,7 @@ public class Hybrid {
 public static void main(String[] args) {
 	p3 p = new p3();
 	p.demo();
+	p3 s = new p3();
+	s.demos();
 }
 }

854e06de309eea2ecb0bb3778126a5af5416b975
mandaladilleswari27-dot <mandaladilleswari27@gmail.com>
2026-09-05 21:35:49 +0530

    Add files via upload

---
+++ Observationprograms/NPE.java
@@ -0,0 +1,9 @@
+class Student{
+    String name;
+}
+public class NPE{
+    public static void main (String[] args){
+        Student s = null;
+        System.out.println(s.name);
+    }
+}
---
+++ Observationprograms/String.java
@@ -0,0 +1,14 @@
+class StringIndexOutOfBound_Demo
+{
+    public static void main(String args[])
+    {
+        try {
+            String a = "My name is likitha"; // length is 15
+            char c = a.charAt(20); // accessing 20th element
+            System.out.println(c);
+        }
+        catch(StringIndexOutOfBoundsException e) {
+            System.out.println("StringIndexOutOfBoundsException");
+        }
+    }
+}

2e07d1df701a53f6f872be10522ffb4846e87522
mandaladilleswari27-dot <mandaladilleswari27@gmail.com>
2026-09-10 21:07:03 +0530

    Add files via upload

---
+++ Assignmentprograms/Uppercase.java
@@ -0,0 +1,14 @@
+class Uppercase {
+    
+    public static void main(String[] args) {
+        String str1 = "hello";
+        String str2 = "java";
+        String str3 = "world";
+
+        System.out.println(str1.toUpperCase());
+        System.out.println(str2.toUpperCase());
+        System.out.println(str3.toUpperCase());
+    }
+}
+
+

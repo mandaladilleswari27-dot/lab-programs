aa7a4c7d81a0a81a2e40a6567c6d29e2fc514c9c
mandaladilleswari27-dot <mandaladilleswari27@gmail.com>
2026-09-10 21:10:16 +0530

    Add files via upload

---
+++ Assignmentprograms/Equal.java
@@ -0,0 +1,13 @@
+ class Equal {
+
+    public static void main(String[] args) {
+        String str1 = "Java";
+        String str2 = "Java";
+        String str3 = "Python";
+
+        System.out.println(str1.equals(str2));
+        System.out.println(str1.equals(str3));
+        System.out.println(str2.equals(str3));
+    }
+}
+

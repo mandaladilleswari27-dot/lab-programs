0ea7c8d1fcbf6f86c2e94f5e3b884dd1c6f6272b
mandaladilleswari27-dot <mandaladilleswari27@gmail.com>
2026-09-10 21:06:28 +0530

    Add files via upload

---
+++ Assignmentprograms/Length.java
@@ -0,0 +1,13 @@
+class Length {
+
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "Java";
+        String str3 = "Programming";
+
+        System.out.println("Length of str1: " + str1.length());
+        System.out.println("Length of str2: " + str2.length());
+        System.out.println("Length of str3: " + str3.length());
+    }
+}
+

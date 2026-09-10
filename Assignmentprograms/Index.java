2dd2c0533010e92a8de8e7a7e9d7fe49dd0c7182
mandaladilleswari27-dot <mandaladilleswari27@gmail.com>
2026-09-10 21:11:12 +0530

    Add files via upload

---
+++ Assignmentprograms/Index.java
@@ -0,0 +1,13 @@
+ class Index {
+
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "Java";
+        String str3 = "Programming";
+
+        System.out.println(str1.indexOf('l'));
+        System.out.println(str2.indexOf('v'));
+        System.out.println(str3.indexOf("gram"));
+    }
+}
+

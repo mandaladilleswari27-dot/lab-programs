069162b4d44c9830942f13b653df2b45c6bf349e
mandaladilleswari27-dot <mandaladilleswari27@gmail.com>
2026-09-10 21:12:16 +0530

    Add files via upload

---
+++ Assignmentprograms/Replace.java
@@ -0,0 +1,13 @@
+class Replace {
+
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "Java";
+        String str3 = "World";
+
+        System.out.println(str1.replace('l', 'x'));
+        System.out.println(str2.replace('a', 'o'));
+        System.out.println(str3.replace('o', 'a'));
+    }
+}
+

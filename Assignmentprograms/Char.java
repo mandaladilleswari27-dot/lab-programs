c80dded04a84c8c7341d2aa32ba602921afec20a
mandaladilleswari27-dot <mandaladilleswari27@gmail.com>
2026-09-10 21:08:28 +0530

    Add files via upload

---
+++ Assignmentprograms/Char.java
@@ -0,0 +1,11 @@
+ class StringOperation2 {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "Java";
+        String str3 = "World";
+
+        System.out.println(str1.charAt(0));
+        System.out.println(str2.charAt(1));
+        System.out.println(str3.charAt(2));
+    }
+}

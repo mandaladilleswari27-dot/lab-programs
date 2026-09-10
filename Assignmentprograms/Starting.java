75fa9b56750c47e56068d214831e0971d3f85980
mandaladilleswari27-dot <mandaladilleswari27@gmail.com>
2026-09-10 21:12:42 +0530

    Add files via upload

---
+++ Assignmentprograms/Starting.java
@@ -0,0 +1,13 @@
+ class Starting {
+
+    public static void main(String[] args) {
+        String str1 = "Hello World";
+        String str2 = "Java Programming";
+        String str3 = "Hello Java";
+
+        System.out.println(str1.startsWith("Hello"));
+        System.out.println(str2.startsWith("Java"));
+        System.out.println(str3.startsWith("World"));
+    }
+}
+

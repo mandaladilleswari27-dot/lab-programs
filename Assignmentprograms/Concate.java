c509e0b9b7a8bdb107269460bbc1caa1c67b5d01
mandaladilleswari27-dot <mandaladilleswari27@gmail.com>
2026-09-10 21:09:22 +0530

    Add files via upload

---
+++ Assignmentprograms/Concate.java
@@ -0,0 +1,13 @@
+ class Concate {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "Java";
+        String str3 = "World";
+
+        System.out.println(str1.concat(str2));
+        System.out.println(str2.concat(str3));
+        System.out.println(str1.concat(str3));
+    }
+}
+
+

721bbf163838d7504cb9656a291262f40f46ac8a
mandaladilleswari27-dot <mandaladilleswari27@gmail.com>
2026-09-10 21:09:54 +0530

    Add files via upload

---
+++ Assignmentprograms/End.java
@@ -0,0 +1,14 @@
+ class End {
+    
+    public static void main(String[] args) {
+        String str1 = "Hello World";
+        String str2 = "Java Programming";
+        String str3 = "Hello Java";
+
+        System.out.println(str1.endsWith("World"));
+        System.out.println(str2.endsWith("Java"));
+        System.out.println(str3.endsWith("Java"));
+    }
+}
+
+

47eb56c26c1e2141cffa7017452403fa63bfe195
mandaladilleswari27-dot <mandaladilleswari27@gmail.com>
2026-09-10 21:13:50 +0530

    Add files via upload

---
+++ Assignmentprograms/Test.java
@@ -0,0 +1,14 @@
+ class Test {
+    
+    public static void main(String[] args) {
+        String str1 = "Hello World";
+        String str2 = "Java Programming";
+        String str3 = "Hello Java";
+
+        System.out.println(str1.contains("World"));
+        System.out.println(str2.contains("Java"));
+        System.out.println(str3.contains("Python"));
+    }
+}
+
+

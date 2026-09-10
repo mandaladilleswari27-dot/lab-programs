c7bf654be5922ca6bbbbd88739748c636434223e
mandaladilleswari27-dot <mandaladilleswari27@gmail.com>
2026-09-10 21:14:17 +0530

    Add files via upload

---
+++ Assignmentprograms/Trim.java
@@ -0,0 +1,13 @@
+class Trim {
+
+    public static void main(String[] args) {
+        String str1 = "  Hello  ";
+        String str2 = "  Java  ";
+        String str3 = "  World  ";
+
+        System.out.println(str1.trim());
+        System.out.println(str2.trim());
+        System.out.println(str3.trim());
+    }
+}
+

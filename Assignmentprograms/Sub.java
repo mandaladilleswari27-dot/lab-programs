064ac262922c737d02ebcdffe2996399f42e92d0
mandaladilleswari27-dot <mandaladilleswari27@gmail.com>
2026-09-10 21:13:06 +0530

    Add files via upload

---
+++ Assignmentprograms/Sub.java
@@ -0,0 +1,14 @@
+ class Sub {
+    public static void main(String[] args) {
+        String str1 = "Programming";
+        String str2 = "Computer";
+        String str3 = "Java";
+        
+
+        System.out.println(str1.substring(0,4));
+        System.out.println(str2.substring(0,4));
+        System.out.println(str3.substring(1,3));
+    }
+}
+
+

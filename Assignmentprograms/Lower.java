c1d4f5bba3c3dc6ee098e8356b3ea7102738c4aa
mandaladilleswari27-dot <mandaladilleswari27@gmail.com>
2026-09-10 21:07:56 +0530

    Add files via upload

---
+++ Assignmentprograms/Lower.java
@@ -0,0 +1,14 @@
+ class Lower {
+    
+    public static void main(String[] args) {
+        String str1 = "HELLO";
+        String str2 = "JAVA";
+        String str3 = "WORLD";
+
+        System.out.println(str1.toLowerCase());
+        System.out.println(str2.toLowerCase());
+        System.out.println(str3.toLowerCase());
+    }
+}
+
+
